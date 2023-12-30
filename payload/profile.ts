5589334c525c41d242784c3cb8401df2317ac538
SoohoLee <hyeri0603@naver.com>
2023-12-30 23:58:03 +0900

    update data

--- payload/profile.ts
+++ payload/profile.ts
@@ -1,7 +1,7 @@
 import { faBook, faEnvelope, faPhone, faRss } from '@fortawesome/free-solid-svg-icons';
 import { faBell } from '@fortawesome/free-regular-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
-import image from '../asset/_sample.jpg';
+import image from '../asset/profileImage.jpg';
 import { IProfile } from '../component/profile/IProfile';
 
 const profile: IProfile.Payload = {
