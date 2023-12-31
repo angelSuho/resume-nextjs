dff4a933d8471e4098bee69131884066b994a751
SoohoLee <hyeri0603@naver.com>
2023-12-31 17:24:59 +0900

    update data

--- payload/article.ts
+++ payload/article.ts
@@ -5,7 +5,7 @@ const article: IArticle.Payload = {
 
   list: [
     {
-      content: '(2018.10~2023.01) 강남대학교 학부연구원 연구 논문 목록',
+      content: '(2018.10~2023.01) 이수호 학부연구원 연구 논문 목록',
       href: 'https://agsooho0303.notion.site/Other-Projects-d75766e58e304991a4baaae3a2930657?pvs=4',
     },
   ],
