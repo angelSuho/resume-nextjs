7c5af4a64068562618b4c5607f2cb8d2aa9b01bd
SoohoLee <hyeri0603@naver.com>
2024-02-22 23:01:59 +0900

    update data

--- payload/profile.ts
+++ payload/profile.ts
@@ -36,7 +36,7 @@ const profile: IProfile.Payload = {
     },
     {
       title: 'Portfolio',
-      link: 'https://drive.google.com/file/d/19WYBjLpuMSi7Dy5-ALQFNwWK5AzXQPSn/view?usp=sharing',
+      link: 'https://drive.google.com/file/d/1gsLFur1IEHRi8vC6fFFt4dW1ilFGbwrB/view?usp=sharing',
       icon: faBook,
     },
     {
